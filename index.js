92302d361eb5e7e00ad4b512db7af537d9b1909c
Max <maxnelson997@gmail.com>
2018-09-26 15:32:17 -0600

    hmm

--- index.js
+++ index.js
@@ -79,6 +79,15 @@ app.post('/api/signup', useBodyParser, function(req, res) {
 })
 
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(__dirname + '/client/dist/'));
+    app.get(/.*/, function (req, res) {
+        res.sendFile(__dirname + '/client/dist/index.html');
+    })
+    console.log('prod inbound');
+  }
+  
+
 const PORT = process.env.PORT || 5000;
 console.log('listening on port: ', PORT);
 app.listen(PORT);
